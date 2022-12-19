3288aae3d69a4fa0e01ddc8bdd3fcff0697ce101
ignatmatei <73382815+ignatmatei@users.noreply.github.com>
2022-12-19 07:35:07 +0200

    fara incercare de dbm

--- proiect/src/back/DatabaseMeme/DatabaseMeme/Controllers/dbmController.cs
+++ proiect/src/back/DatabaseMeme/DatabaseMeme/Controllers/dbmController.cs
@@ -1,40 +1,40 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace DatabaseMeme.Controllers
-{
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class dbmController : ControllerBase
-    {
-        [HttpGet]
-        public Useri[] getuser()
-        {
-            using var db = new MemeDbContext();
-            return db.Useris.ToArray();
-        }
-        [HttpGet]
-        public Meme[] getmeme()
-        {
-            using var db = new MemeDbContext();
-            return db.Memes.ToArray();
-        }
-        [HttpGet]
+//namespace DatabaseMeme.Controllers
+//{
+//    [Route("api/[controller]/[action]")]
+//    [ApiController]
+//    public class dbmController : ControllerBase
+//    {
+//        [HttpGet]
+//        public Useri[] getuser()
+//        {
+//            using var db = new MemeDbContext();
+//            return db.Useris.ToArray();
+//        }
+//        [HttpGet]
+//        public Meme[] getmeme()
+//        {
+//            using var db = new MemeDbContext();
+//            return db.Memes.ToArray();
+//        }
+//        [HttpGet]
 
-        public Meme[] getMeme(long memeid)
-        {
-            using var db = new MemeDbContext();
-            var memes = db.Memes.Where(x => x.Id == memeid).ToArray();
-            return memes;
-        }
-        [HttpDelete]
+//        public Meme[] getMeme(long memeid)
+//        {
+//            using var db = new MemeDbContext();
+//            var memes = db.Memes.Where(x => x.Id == memeid).ToArray();
+//            return memes;
+//        }
+//        [HttpDelete]
 
-        public void deleteMeme(long id) {
-            var m = new Meme();
-            m.Id = id;
-            using  var db = new MemeDbContext();
-            db.Memes.Remove(m);
-            db.SaveChanges();
-        }
-    }
-}
+//        public void deleteMeme(long id) {
+//            var m = new Meme();
+//            m.Id = id;
+//            using  var db = new MemeDbContext();
+//            db.Memes.Remove(m);
+//            db.SaveChanges();
+//        }
+//    }
+//}
